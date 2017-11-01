e8d24e725ae8d9b0099e2a1d8604ad907c9dcf37
dss13 <ssumugan007@gmail.com>
2017-11-01 19:11:34 +0530

    added angular to team

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,27 @@
-var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime()
 
 var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var now = new Date().getTime()
+    var distance = countDownDate - now
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-    $("#days").text(days);
-    $("#hours").text(hours);
-    $("#minutes").text(minutes);
-    $("#seconds").text(seconds);
+    $("#days").text(days)
+    $("#hours").text(hours)
+    $("#minutes").text(minutes)
+    $("#seconds").text(seconds)
 
     if (distance < 0) {
-        clearInterval(x);
+        clearInterval(x)
         $("#timer").hide()
     }
-}, 1000);
+}, 1000)
+
+var app = angular.module('app', [])
+app.controller('teamController', function($scope, $http) {
+    $http.get('/data/team.json').then(function(results) {
+        $scope.team = results.data.data
+    })
+})
