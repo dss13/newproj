a096f6e55b6a7d55bb5d5d5df561ccba9c6dfb22
dss13 <ssumugan007@gmail.com>
2017-11-10 11:40:22 +0530

    navbar done

--- public/js/index.js
+++ public/js/index.js
@@ -32,26 +32,38 @@ app.controller('speakersController', function($scope, $http) {
 })
 
 $(document).ready(function(){
+    hashUp()
   // Add smooth scrolling to all links
-  $(".scrollable").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+    $(".scrollable").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault()
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
   });
 });
+
+window.onhashchange = function() {
+    hashUp()
+}
+
+function hashUp() {
+    var hrf = location.hash
+    $('.scrollable').parent().removeClass('active')
+    $('.nav-link[href="' + hrf + '"]').parent().addClass('active')
+}
+
+window.onscroll = function() {
+    var themeH = (document.getElementById('themeDiv').clientHeight - 30) || 0
+    if (window.pageYOffset > themeH) {
+        $('.navbar').removeClass('topNav')
+        $('.navbar').addClass('downNav')
+    } else {
+        $('.navbar').removeClass('downNav')
+        $('.navbar').addClass('topNav')
+    }
+}
