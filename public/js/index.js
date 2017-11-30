5051aff983215c061e260c5ab663d20d50159f99
dss13 <ssumugan007@gmail.com>
2017-11-30 12:32:04 +0530

    improved gallery

--- public/js/index.js
+++ public/js/index.js
@@ -59,7 +59,6 @@ function hashUp() {
 }
 
 window.onscroll = function() {
-    var themeH = (document.getElementById('themeDiv').clientHeight - 30) || 0
     if (window.pageYOffset > 150) {
         $('.navbar').removeClass('topNav')
         $('.navbar').addClass('downNav')
